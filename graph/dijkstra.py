007730480b3dfb6610ff80aacee2eff0dba01653
yang2011 <tori2011@gmail.com>
2015-10-02 01:51:14 -0700

    added bidirectional dijkstra search

--- graph/dijkstra.py
+++ graph/dijkstra.py
@@ -57,4 +57,83 @@ def labeling(g,s,t,wt_attr='weight'):
     return (path,vData)
     
     
+"""
+plain bidirectional Dijkstra
+wt_attr: edge weight attribute name
+"""
+def bidirectionalSearch(g,s,t,wt_attr):
+    gRev= g.reverse()
+    vDataS = {n:{"ds":float('inf'),
+                "parent":None,
+                "status":Status.unreached} for n in g}
+    vDataS[s]['ds'] =0
+    vDataS[s]['status'] = Status.labeled
+    vDataT = {n:{"ds":float('inf'),
+                "parent":None,
+                "status":Status.unreached} for n in g}
+    vDataT[t]['ds'] =0
+    vDataT[t]['status'] = Status.labeled
+    pqueues = []
+    pqueuet = []
+    heappush(pqueues, (vDataS[s]['ds'], s))
+    heappush(pqueuet, (vDataT[t]['ds'], t))
+    ds_top = 0
+    dt_top = 0
+    mu = float('inf') # best shortest path length found so far
+    while (len(pqueues)>0 and len(pqueuet)>0):
+        # forward search
+        (ds_top,v) = heappop(pqueues)
 
+        if ( ds_top + dt_top >= mu ): # stop condition
+            break
+        dv = vDataS[v]['ds']
+        for w in g.neighbors(v):
+            if (relax(g,v,w,wt_attr,dv,vDataS)):
+                heappush(pqueues, (vDataS[w]['ds'],w))
+                
+            if ( vDataT[w]['status'] == Status.scanned and
+                 vDataS[w]['ds'] +vDataT[w]['ds'] < mu):
+                mu = vDataS[w]['ds'] + vDataT[w]['ds']
+        vDataS[v]['status'] =Status.scanned
+        
+        # backward search
+        (dt_top,v) = heappop(pqueuet)
+        if (ds_top + dt_top >= mu): # stop condition
+            break
+        dv = vDataT[v]['ds']
+        for w in gRev.neighbors(v):
+            if (relax(gRev,v,w,wt_attr,dv,vDataT)):
+                heappush(pqueuet, (vDataT[w]['ds'],w))
+
+            if ( vDataS[w]['status'] == Status.scanned and
+                 vDataS[w]['ds'] +vDataT[w]['ds'] < mu):
+                mu = vDataS[w]['ds'] + vDataT[w]['ds']                
+        vDataT[v]['status'] =Status.scanned
+    
+    path = reconstructMinPath(s,t,vDataS,vDataT)
+    return (path,vDataS,vDataT)
+
+""" reconstruct shortest s-t path from forward shortest path tree
+and backward shortest path tree, given midpoint 
+""" 
+def reconstructMinPath(s,t,vDataS,vDataT):
+    midpoints = [(vDataS[v]['ds']+ vDataT[v]['ds'],v)  for v in vDataS.keys() if \
+                 (vDataS[v]['status'] != Status.unreached and \
+                  vDataT[v]['status'] != Status.unreached)]
+
+    (minPathLen,midpoint) = min(midpoints)
+    print "shortest path distance: " , minPathLen, " with midpoint " , midpoint
+    
+    path = []
+    u = midpoint
+    while (vDataS[u]['parent']!=None):
+        path.insert(0,u)
+        u = vDataS[u]['parent']
+    path.insert(0,s)
+    
+    u = vDataT[midpoint]['parent']
+    while (vDataT[u]['parent']!=None):
+        path.append(u)
+        u = vDataT[u]['parent']
+    path.append(t)
+    return path
